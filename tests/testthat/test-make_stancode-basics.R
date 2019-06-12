661c3ea49c69d1302d42c3e3272271868b4ba976
Henrik Singmann <singmann@gmail.com>
2019-06-12 16:58:16 +0100

    added tests for TRMS and b_MAP

--- tests/testthat/test-make_stancode-basics.R
+++ tests/testthat/test-make_stancode-basics.R
@@ -18,5 +18,27 @@ test_that("formulas with explicit or missing intercept are handled correctly", {
 
 })
 
+test_that("works with multiple fixed effects", {
+  ## load("tests/testthat/fhchr.rda")
+  load("fhchr.rda")
+
+  d1 <- make_standata_bfrms(rt ~ 1 + (length||id), fhchr)
+  expect_true("r_random" %in% names(d1))
+  d2 <- make_standata_bfrms(rt ~ task + (length||id), fhchr)
+  expect_identical(d2$TRMS, 1L)
+  expect_identical(d2$b_MAP, 1L)
+
+  d3 <- make_standata_bfrms(rt ~ task + length + (length||id), fhchr)
+  expect_identical(d3$TRMS, 2L)
+  expect_identical(d3$b_MAP, c(1L, 2L, 2L))
+
+  d4 <- make_standata_bfrms(rt ~ length + task + (length|id), fhchr)
+  expect_identical(d4$TRMS, 2L)
+  expect_identical(d4$b_MAP, c(1L, 1L, 2L))
+
+  d5 <- make_standata_bfrms(rt ~ length * task + (length|id), fhchr)
+  expect_identical(d5$TRMS, 3L)
+  expect_identical(d5$b_MAP, c(1L, 1L, 2L, 3L, 3L))
+})
 
 
